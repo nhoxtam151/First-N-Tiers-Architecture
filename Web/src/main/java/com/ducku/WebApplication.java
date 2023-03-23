eaa5ff94badd8d70d6fb01d1a39bffee6b5fa585
nhoxtam151 <=huynhductam69@gmail.com>
2023-03-23 18:45:34 +0700

    adding component scan in webApllication

--- Web/src/main/java/com/ducku/WebApplication.java
+++ Web/src/main/java/com/ducku/WebApplication.java
@@ -2,10 +2,13 @@ package com.ducku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.ducku.*"})
+@EntityScan(basePackages = {"com.ducku.*"})
+//@EnableJpaRepositories(basePackages = {"com.ducku.*"})
 public class WebApplication {
 
   public static void main(String[] args) {
